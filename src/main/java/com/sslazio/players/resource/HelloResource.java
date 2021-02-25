5723bf0eb4b59d05b97f96fbc5499cb7d6a2d8d4
geralt1989 <raffraioli@gmail.com>
2021-02-25 16:46:12 +0100

    update pom and app prop

--- src/main/java/com/sslazio/players/resource/HelloResource.java
+++ src/main/java/com/sslazio/players/resource/HelloResource.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/rest/hello")
+@RequestMapping(path="/ap1/rest/hello")
 public class HelloResource {
 
     @GetMapping
